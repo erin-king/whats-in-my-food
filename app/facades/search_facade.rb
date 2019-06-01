9eabe1403c887084d349da6891f483a47fa229fa
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-01 10:18:01 -0600

    Refactored search_facade #service

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -4,26 +4,21 @@ class SearchFacade
   end
 
   def foods
-    service = UsdaService.new
     food_results = service.get_foods(@criteria)
 
     food_results[:item].map do |food_data|
       Food.new(food_data)
     end
-    #
-    # conn = Faraday.new("https://api.nal.usda.gov/ndb/search/") do |f|
-    #   f.params['api_key'] = ENV['food_key']
-    #   f.adapter Faraday.default_adapter
-    # end
-    # response = conn.get("?format=json&q=sweet potatoes&sort=n&max=10&offset=0")
-    # food_results = JSON.parse(response.body, symbolize_names: true)[:list]
-    # food_results[:item].map do |food_data|
-    #   Food.new(food_data)
-    # end
   end
 
   def food_count
     "#{foods.count} Items"
+  end
+
+  private
+
+  def service
+    @_service ||= UsdaService.new  
   end
 end
 # DON'T FORGET FOOD_COUNT!!! AND TEST LINE 14-16
